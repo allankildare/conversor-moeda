440d2236e76e153f9a02bc64715d4d359a68ab6a
Allan Kildare <allankildare27@gmail.com>
2020-10-15 02:03:26 -0300

    primeiro commit :)

---
+++ src/components/Conversor.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import './Conversor.css'
+
+export default class Conversor extends Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            moedaA_valor: "",
+            moedaB_valor: 0
+        }
+        this.converter = this.converter.bind(this)
+    }
+
+    converter () {
+        let de_para = `${this.props.moedaA}-${this.props.moedaB}`
+        let de = `${this.props.moedaA}`
+        let url = `https://economia.awesomeapi.com.br/json/all/${de_para}`
+
+        fetch(url)
+            .then(res => {
+                return res.json()
+            })
+            .then(json => {
+                let cotacao = json[de].low
+                let moedaB_valor = (parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2)
+                this.setState({moedaB_valor})
+        })
+    }
+
+    render() {
+        return (
+            <div className="conversor">
+                <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
+                <input type="text" onChange={(event) => {this.setState({moedaA_valor:event.target.value})}} />
+                <input type="button" value="converter" onClick={this.converter} />
+                <h2>{this.state.moedaB_valor}</h2>
+            </div>
+        )
+    }
+}
